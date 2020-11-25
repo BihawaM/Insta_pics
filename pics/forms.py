3aec2c4fd66b55532bf175501d55cac0f879d44c
BihawaM <bihawam271@gmail.com>
2020-11-25 05:41:07 +0300

    add form classes

--- pics/forms.py
+++ pics/forms.py
@@ -1,33 +1,33 @@
-# from django import forms
-# from .models import *
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import User
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ('image', 'caption')
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('image', 'caption')
 
-# class SignUpForm(UserCreationForm):
-#     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'password1', 'password2')
-
-
-# class ProfileForm(forms.ModelForm):
-#    class Meta:
-#         model = Profile
-#         fields = ['name', 'profile_picture', 'bio']
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
-# class CommentForm(forms.ModelForm):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['text'].widget=forms.TextInput()
-#         self.fields['text'].widget.attrs['placeholder']='Add a comment...'
+class ProfileForm(forms.ModelForm):
+   class Meta:
+        model = Profile
+        fields = ['name', 'profile_picture', 'bio']
 
-#     class Meta:
-#         model = Comment
-#         fields = ('text',)
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget=forms.TextInput()
+        self.fields['text'].widget.attrs['placeholder']='Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
